b4529ebe6acb6d3e676c47b78c071348c8aa6ae1
NadiaAiraf <aidanfaria@gmail.com>
2018-09-21 12:14:48 +0100

    added feature test for player 1s hitpoints as well as player 2

--- spec/features/hitpoints_spec.rb
+++ spec/features/hitpoints_spec.rb
@@ -4,4 +4,8 @@ feature "displays hit points" do
     sign_in_and_play
     expect(page).to have_content("John: 50HP")
   end
+  scenario "See player 1's hit points" do
+    sign_in_and_play
+    expect(page).to have_content("Jim: 50HP")
+  end
 end
